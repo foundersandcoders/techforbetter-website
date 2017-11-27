ebcef97ab248e423883073ec5f545a22b661416d
Jamie Coe <jamiecoe66@gmail.com>
2017-11-27 09:51:52 +0000

    added logo grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,3 +90,9 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
