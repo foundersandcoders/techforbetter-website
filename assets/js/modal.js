09c70fd3d870f0fff0d465d47862916b0e72570a
sima-qian <29804933+sima-qian@users.noreply.github.com>
2018-07-25 23:42:59 +0100

    adds clicking elsewhere in modal closes modal feature

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -85,3 +85,9 @@ function imageModal(image) {
 closeModal.onclick = function() {
   modal.style.display = "none";
 };
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
